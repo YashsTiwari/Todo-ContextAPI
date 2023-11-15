86813ab6a37c9ea031df710c50bd4816e2889e75
Yashs Tiwari <yashstiwari03@gmail.com>
2023-11-15 22:23:29 +0530

    project initialized

---
+++ src/context/TodoContext.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const TodoContext = React.createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo = () => {
+  return React.useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
