140ccc58c1f96d392d00d5594ff042090f8d4c73
Yashs Tiwari <yashstiwari03@gmail.com>
2023-11-17 11:09:59 +0530

    added localStorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { TodoProvider } from "./context";
 
 function App() {
-  const [todo, setTodo] = React.useState([]);
+  const [todos, setTodos] = React.useState([]);
 
   const addTodo = (todo) => {
-    setTodo((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
 
   const updateTodo = (id, todo) => {
-    setTodo((prev) =>
+    setTodos((prev) =>
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
     );
   };
   const deleteTodo = (id) => {
-    setTodo((prev) => prev.filter((todo) => todo.id !== id));
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
 
   const toggleComplete = (id) => {
-    setTodo((prev) =>
+    setTodos((prev) =>
       prev.map((prevTodo) =>
         prevTodo.id === id
           ? { ...prevTodo, completed: !prevTodo.completed }
@@ -27,6 +27,17 @@ function App() {
       )
     );
   };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) setTodos(todos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <TodoProvider
       value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete }}
